c116f54e7c77375568e24dc35ce5170eaf53551a
Arman Rittik Sheikh <rittikwork@gmail.com>
2024-04-19 14:37:26 +0100

    Update navbar search form

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -1,8 +1,29 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, NavLink, useNavigate, useLocation } from "react-router-dom";
 import { UseSelector, useSelector } from "react-redux";
+import { FaSearch } from "react-icons/fa";
 function Navbar() {
   const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
+
   return (
     <header className="bg-black">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -12,12 +33,20 @@ function Navbar() {
             <span className="text-orange-600">Fleet</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-3 rounded-lg flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center"
+        >
           <input
             type="text"
             placeholder="Search"
             className="bg-transparent focus:outline-none w-24 sm:w-64 text-black"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
+          <button>
+            <FaSearch className="text-slate-600" />
+          </button>
         </form>
 
         <ul className="flex gap-4">
